8ec563c1672068e102ba63f62c1f2e5c0a6e2373
Gilles Rusca <gilles.rusca@gmail.com>
2021-02-20 11:37:32 -0600

    added gatsby

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className='container-fluid'>
+    <button type='button' className='btn btn-primary'>Primary</button>
+  </div>
+);
+
+export default Home;
