f499f1d9272827f986173a94982f8404953a953d
Gilles Rusca <gilles.rusca@gmail.com>
2021-02-20 14:49:35 -0600

    displaying data

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const Home = () => (
-  <div className='container-fluid'>
-    <button type='button' className='btn btn-primary'>Primary</button>
-  </div>
-);
+const Home = ({ data }) => {
+  console.log({ data });
+  return (
+    <>
+      <h2>{data.allSanityCompany.nodes.map((company) => company.name)}</h2>
+      <div className='container-fluid'>
+        <button type='button' className='btn btn-primary'>Primary</button>
+      </div>
+    </>
+  );
+};
 
 export default Home;
+
+export const query = graphql`
+  query CompanyQuery {
+    allSanityCompany {
+      nodes {
+        name
+      }
+    }
+  }
+`;
