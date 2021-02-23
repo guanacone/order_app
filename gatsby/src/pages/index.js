d3a0573f283fa850142f6c5e9669c05d8c1b14e9
Gilles Rusca <gilles.rusca@gmail.com>
2021-02-23 09:21:22 -0600

    removed button

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -6,9 +6,6 @@ const Home = ({ data }) => {
   return (
     <>
       <h2>{data.allSanityCompany.nodes.map((company) => company.name)}</h2>
-      <div className='container-fluid'>
-        <button type='button' className='btn btn-primary'>Primary</button>
-      </div>
     </>
   );
 };
