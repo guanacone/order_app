3c9a94c2325e97a8cb9d4fba5f8e3cca9382fb86
Gilles Rusca <gilles.rusca@gmail.com>
2021-02-24 14:24:50 -0600

    added pages

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
-const Home = ({ data }) => {
-  console.log({ data });
+const Home = () => {
   return (
-    <>
-      <h2>{data.allSanityCompany.nodes.map((company) => company.name)}</h2>
-      <p>This is a test to see if content is pulled correctly from sanity.</p>
-    </>
+    <h2>I&apos;m the home page and will display all pizzas</h2>
   );
 };
 
 export default Home;
-
-export const query = graphql`
-  query CompanyQuery {
-    allSanityCompany {
-      nodes {
-        name
-      }
-    }
-  }
-`;
---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrderPage = () => {
+  return (
+    <h2>I&apos;m the order page and will display the order form</h2>
+  );
+};
+
+export default OrderPage;
