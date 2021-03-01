5dd8412e7dd277835ac4243fd03a0df12e6000d0
Gilles Rusca <gilles.rusca@gmail.com>
2021-03-01 15:34:31 -0600

    added error handeling

--- gatsby/src/utils/useOrder.js
+++ gatsby/src/utils/useOrder.js
@@ -40,28 +40,33 @@ const useOrder = ({ pizzas, values }) => {
       name: values.name,
       email: values.email,
     };
-    const res = await fetch(
-      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    try {
+      const res = await fetch(
+        `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
         },
-        body: JSON.stringify(body),
-      },
-    );
-    const text = JSON.parse(await res.text());
+      );
+      const text = JSON.parse(await res.text());
 
-    // check if everything worked
-    if (res.status >= 400 && res.status < 600) {
+      // check if everything worked
+      if (res.status >= 400 && res.status < 600) {
+        setLoading(false); // turn off loading
+        setError(text.message);
+      } else {
+        // it worked!
+        setLoading(false);
+        setMessage('Success! Come on down for your pizza');
+        setOrder([]);
+        setTimeout(() => setMessage(null), 2e3);
+      }
+    } catch (err) {
       setLoading(false); // turn off loading
-      setError(text.message);
-    } else {
-      // it worked!
-      setLoading(false);
-      setMessage('Success! Come on down for your pizza');
-      setOrder([]);
-      setTimeout(() => setMessage(null), 2e3);
+      setError('Internal Server Error. Please try again!');
     }
   };
 
